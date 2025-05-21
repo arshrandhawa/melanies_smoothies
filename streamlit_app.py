be5a99cd73b653194409cc0df366a4005d50c9eb
Arshdeeep Randhawa <arshdip.randhawa@gmail.com>
2025-05-21 13:46:51 -0700

    Update streamlit_app.py
    adding search_on function

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,10 @@ name_on_order = st.text_input("Name on Smoothie:")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+pd_df = my_dataframe_to_pandas()
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = ''
 ingredients_list = st.multiselect(
@@ -30,7 +33,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.subheader(fruit_choosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
